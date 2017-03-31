14d7bdd19b50463e76ac38986fd7b0a16f05422d
CarsonTian <tjq19911017@gmail.com>
2017-03-31 20:21:27 +1100

    change

--- app/src/main/java/com/example/greyson/test1/net/NetWorkApi.java
+++ app/src/main/java/com/example/greyson/test1/net/NetWorkApi.java
@@ -68,7 +68,4 @@ public class NetWorkApi {
         }
         return mRetrofit;
     }
-
-
-
 }
--- app/src/main/java/com/example/greyson/test1/ui/fragment/SafetyMapFragment.java
+++ app/src/main/java/com/example/greyson/test1/ui/fragment/SafetyMapFragment.java
@@ -13,6 +13,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 import com.example.greyson.test1.R;
 import com.example.greyson.test1.ui.base.BaseFragment;
@@ -39,6 +41,8 @@ public class SafetyMapFragment extends BaseFragment implements GoogleApiClient.C
     //private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
     private LocationRequest mLocationRequest;
     private Context mContext;
+    private Spinner spinner;
+    private int changeState;
     MapView mapView;
 
     @Override
@@ -64,13 +68,6 @@ public class SafetyMapFragment extends BaseFragment implements GoogleApiClient.C
                 googleMap = mMap;
 
                 if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
                     return;
                 }
                 googleMap.setMyLocationEnabled(true);
@@ -93,6 +90,29 @@ public class SafetyMapFragment extends BaseFragment implements GoogleApiClient.C
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                 .setInterval(2000)        // 1 seconds, in milliseconds
                 .setFastestInterval(1000); // 1 second, in milliseconds
+
+
+        spinner = (Spinner) view.findViewById(R.id.spinner);
+        spinner.setVisibility(View.VISIBLE);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+                if (pos == 0) {
+                    changeState = 1;
+                }
+                if (pos == 1) {
+                    changeState = 2;
+                }
+                if (pos == 2) {
+                    changeState = 3;
+                }
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
         return view;
     }
 
@@ -126,41 +146,31 @@ public class SafetyMapFragment extends BaseFragment implements GoogleApiClient.C
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         Log.i(TAG, "Location services connected.");
-
         if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
-
         LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
         Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        boolean range = false;
-        selectLocation(location, range);
+        spinnerFunction(location);
     }
 
+
     private void selectLocation(Location location, boolean range) {
-        if (compareLocation(location, 2000, range) == false) {
+        if (!compareLocation(location, 2000, range)) {
             compareLocation(location, 5000, range);
         }
     }
 
     public boolean compareLocation(Location location, int dis, boolean range) {
 
-        String json = "[{ \"names\":\"Police1\",\"lai\":\"-37.924404\",\"lon\":\"145.120902\" },{ \"names\":\"Police2\",\"lai\":\"-37.895275\",\"lon\":\"145.125083\"  }]";
+        String json = "[{\"id\":1,\"establishment\":\"McDonald's\",\"address\":\"1500 Eastlink Northbound\",\"suburb\":\"Scoresby\",\"postcode\":3179,\"state\":\"VIC\",\"type\":\"Restaurant\",\"latitude\":\"-37.9123\",\"longtitude\":\"145.2135\"},{\"id\":2,\"establishment\":\"McDonald's\",\"address\":\"Princes Fwy\",\"suburb\":\"Officer\",\"postcode\":3809,\"state\":\"VIC\",\"type\":\"Restaurant\",\"latitude\":\"-38.0608\",\"longtitude\":\"145.4134\"},{\"id\":3,\"establishment\":\"McDonald's\",\"address\":\"127 Canterbury Rd\",\"suburb\":\"Blackburn South\",\"postcode\":3130,\"state\":\"VIC\",\"type\":\"Restaurant\",\"latitude\":\"-37.8316\",\"longtitude\":\"145.1478\"},{\"id\":4,\"establishment\":\"McDonald's\",\"address\":\"Burwood Hwy & Scott Grove\",\"suburb\":\"Burwood\",\"postcode\":3125,\"state\":\"VIC\",\"type\":\"Restaurant\",\"latitude\":\"-37.8506\",\"longtitude\":\"145.0980\"},{\"id\":5,\"establishment\":\"McDonald's\",\"address\":\"606 Warrigal Rd\",\"suburb\":\"Holmesglen\",\"postcode\":3148,\"state\":\"VIC\",\"type\":\"Restaurant\",\"latitude\":\"-37.8759\",\"longtitude\":\"145.0910\"},{\"id\":6,\"establishment\":\"McDonald's\",\"address\":\"North Rd & Poath Road\",\"suburb\":\"Oakleigh\",\"postcode\":3166,\"state\":\"VIC\",\"type\":\"Restaurant\",\"latitude\":\"-37.9081\",\"longtitude\":\"145.0742\"}]";
         try {
             JSONArray jsonArray = new JSONArray(json);
             for (int i = 0; i < jsonArray.length(); i++) {
-
                 JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-                String name = jsonObject.getString("names");
-                double lai = Double.parseDouble(jsonObject.getString("lai"));
-                double lon = Double.parseDouble(jsonObject.getString("lon"));
+                String name = jsonObject.getString("establishment");
+                double lai = Double.parseDouble(jsonObject.getString("latitude"));
+                double lon = Double.parseDouble(jsonObject.getString("longtitude"));
                 if (getDistance(handleNewLocation(location).latitude, handleNewLocation(location).longitude, lai, lon) < dis) {
                     addMark(lai, lon, name);
                     range = true;
@@ -191,6 +201,17 @@ public class SafetyMapFragment extends BaseFragment implements GoogleApiClient.C
         return results[0];
     }
 
+    public void spinnerFunction(Location location) {
+        boolean range = false;
+        selectLocation(location, range);
+        if (changeState == 2) {
+        googleMap.clear();
+        handleNewLocation(location);
+        }
+        if (changeState == 3) {
+        }
+    }
+
     @Override
     public void onConnectionSuspended(int i) {
         Log.i(TAG, "Location services suspended. Please reconnect.");
@@ -203,9 +224,27 @@ public class SafetyMapFragment extends BaseFragment implements GoogleApiClient.C
 
     @Override
     public void onLocationChanged(Location location) {
-        boolean range = false;
         googleMap.clear();
-        selectLocation(location, range);
+        spinnerFunction(location);
     }
+
+    /**@Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (position == 1) {
+            changeState = 1;
+        }
+        if (position == 2) {
+            changeState = 2;
+        }
+        if (position == 3) {
+            changeState = 3;
+        }
+        //mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        changeState = 1;
+    }*/
 }
 
